5c33fb29941a070311cb743a78416be5e02a49fc
Paschal Ngoi <47353288+ngoijr3107@users.noreply.github.com>
2022-12-23 20:02:44 +0300

    department chart

---
+++ resources/js/components/DashboardCharts/DepartmentComplaintChart.vue
@@ -0,0 +1,42 @@
+<template>
+
+    <div>
+        <apexchart width="500" type="line" :options="options" :series="series"></apexchart>
+    </div>
+
+</template>
+                  
+
+<script>
+import axios from 'axios';
+import VueApexCharts from 'vue-apexcharts';
+
+export default {
+    components: {
+        VueApexCharts,
+    },
+    data() {
+        return {
+            options: {
+                chart: {
+                    id: 'vuechart-example'
+                },
+                xaxis: {
+                    categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
+                }
+            },
+            series: [{
+                name: 'series-1',
+                data: [30, 40, 45, 50, 49, 60, 70, 91]
+            }]
+        }
+    },
+    methods: {
+
+        
+    },
+    mounted() {
+      
+    }
+}
+</script>
