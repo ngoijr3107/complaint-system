c0c20f97aaefa01f34c9cf36b869296048776b5c
Paschal Ngoi <47353288+ngoijr3107@users.noreply.github.com>
2022-12-23 20:03:03 +0300

    sites chart

---
+++ resources/js/components/DashboardCharts/SiteComplaintChart.vue
@@ -0,0 +1,49 @@
+<template>
+
+    <div id="chart">
+        <apexchart type="pie" width="460" :options="chartOptions" :series="series"></apexchart>
+    </div>
+
+</template>
+                  
+
+<script>
+import axios from 'axios';
+import VueApexCharts from 'vue-apexcharts';
+
+export default {
+    components: {
+        VueApexCharts,
+    },
+    data() {
+        return {
+            series: [44, 55, 13, 43, 22],
+            chartOptions: {
+                chart: {
+                    width: 380,
+                    type: 'pie',
+                },
+                labels: ['Team A', 'Team B', 'Team C', 'Team D', 'Team E'],
+                responsive: [{
+                    breakpoint: 480,
+                    options: {
+                        chart: {
+                            width: 200
+                        },
+                        legend: {
+                            position: 'bottom'
+                        }
+                    }
+                }]
+            }
+        }
+    },
+        methods: {
+
+
+        },
+        mounted() {
+
+        }
+    }
+</script>
