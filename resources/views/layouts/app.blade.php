b16a356e1f28a2d42354c3d486469a455fac8ae3
Paschal Ngoi <47353288+ngoijr3107@users.noreply.github.com>
2022-12-23 16:19:51 +0300

    favicon change

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <link rel="icon" href="/images/logo_totalenergies.webp">
+    <link rel="icon" href="/images/favicon.png">
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
